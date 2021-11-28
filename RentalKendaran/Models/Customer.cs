de2a5e7a23fb0bb00c6f1f9659a83dc37dff0726
Aldi-nafi43 <aldi.nafi43@gmail.com>
2021-11-28 13:42:42 +0700

    commit

--- RentalKendaran/Models/Customer.cs
+++ RentalKendaran/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -13,9 +14,17 @@ namespace RentalKendaran.Models
         }
 
         public int IdCustomer { get; set; }
+        [Required(ErrorMessage = "Nama Customer tidak boleh Kosong")]
         public string NamaCustomer { get; set; }
+        [MinLength(10, ErrorMessage = "NO hp min 16 angka")]
+        [MaxLength(13, ErrorMessage = "NO hp max 16 angka")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "hanya boleh diisi oleh angka")]
         public string Nik { get; set; }
+        [Required(ErrorMessage = "Alamat tidak boleh Kosong")]
         public string Alamat { get; set; }
+        [MinLength(10, ErrorMessage = "NO hp min 10 angka")]
+        [MaxLength(13, ErrorMessage = "NO hp max 13 angka")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "hanya boleh diisi oleh angka")]
         public string NoHp { get; set; }
         public int? IdGender { get; set; }
 
